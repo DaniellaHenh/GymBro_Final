ebcf5c5e4750cfeb719090d0cf292dafa8947531
DaniellaHenh <daniellahamo23@gmail.com>
2025-05-12 22:08:57 +0300

    fix: remove unnecessary whitespace in onAuthStateChanged callback

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
       setLoading(false);
+      
     });
 
     return () => unsubscribe();
